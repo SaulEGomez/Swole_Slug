1246da9b9c8c041aadc438e683a9a945d93c2c17
ElSaulGomez <147011501+ElSaulGomez@users.noreply.github.com>
2025-03-30 18:35:58 -0700

    Defined database model

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,12 @@ load_dotenv()
 
 app = FastAPI()
 
+from models.user import Base
+from database import engine
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def root():
     return {"message": "Welcome to SwoleSlug API"}
